4702d8fd9b19e17a6e74c75b3881c180255192a5
saffronl <51935852+saffronl@users.noreply.github.com>
2020-11-19 20:50:43 -0500

    Update ReadingFromExcel.py

--- ReadingFromExcel.py
+++ ReadingFromExcel.py
@@ -1,47 +1,54 @@
 # Saffron Livaccari
 
-#pip install pandas
-#pip install xlrd
-#pip install openpyxl
 import pandas as pd
 import numpy as np
 import statistics as st
 import datetime as dt
-import xlrd
 
-## For other excel files - all of the column names will have to change to fit the new excel sheet
-#   'sample.date', 'parameter', 'loc.ID', 'data.value'
+### Three Lines have to be changed - marked by ###
 
-#### Reading from the excel sheet
+# To make the important column names universal
+def ExcelSheet(file,ColumnNameforDate,ColumnNameforBacteria,ColumnNameforLocation,ColumnforData):
+    data = pd.read_excel(file)
+    df = pd.DataFrame(data)
+    df.rename(columns={ColumnNameforDate:'Date',ColumnNameforBacteria:'Bacteria',
+                         ColumnNameforLocation:'Location',ColumnforData:'Data'},inplace=True)
+    df = df.dropna(subset=['Date','Bacteria','Location','Data'])
+    df.to_excel('data.xlsx')
+    print('DataFrame is written to Excel File successfully.')
+    return df
 
-## PWD Data
-#data = pd.read_excel(r'/Users/saffron/Documents/Howard_Neukrug_Water_Center_PWD_Shore_Grab_Bacteria_2019.xlsx')
+### CHANGE THIS TO FIT EXCEL SHEET
+#DRBC
+excel = '/Users/saffron/Downloads/Nearshore_Bacteria.xlsx'
+#PWD
+#excel = '/Users/saffron/Documents/Howard_Neukrug_Water_Center_PWD_Shore_Grab_Bacteria_2019.xlsx'
+### CHANGE THIS TO FIT THE COLUMNS NAMES OF ABOVE EXCEL SHEET
+#DRBC
+ExcelSheet(excel,'ActivityStartDate','CharacteristicName','Location','ResultMeasureValue')
+#PWD
+#ExcelSheet(excel,'sample.date', 'parameter', 'loc.ID', 'data.value')
 
-## DRBC Data
-data = pd.read_excel(r'/Users/saffron/Downloads/Nearshore_Bacteria.xlsx',sheet_name = "Raw Data")
-
+#Excel Sheet made from function
+data = pd.read_excel(r'/Users/saffron/PycharmProjects/PythonProject1/data.xlsx')
 #Excel sheet for the precipiation
-raindata = pd.read_excel(r'/Users/saffron/Documents/Philadelphia Airport Precip data 2019.xlsx')
-
-#### Create a pandas dataframe
-df = pd.DataFrame(data)
+raindata = pd.read_excel(r'/Users/saffron/PycharmProjects/PythonProject1/RainData.xlsx')
 df1 = pd.DataFrame(raindata)
-
-#### Remove missing data from IMPORTANT columns
-
-## PWD Data
-#df = df.dropna(subset=['sample.date', 'parameter', 'loc.ID', 'data.value'])
-
-## DRBC Data
-df = df.dropna(subset=['ActivityStartDate', 'CharacteristicName', 'Location', 'ResultMeasureValue'])
 
 def dataframe_to_dict(df, key_column, value_column):
     dict = df.set_index(key_column)[value_column].to_dict()
     return dict
 
-
 rain_dict = dataframe_to_dict(df1,'DATE','PRCP')
-print(rain_dict)
+TwoDays_dict = dataframe_to_dict(df1,'DATE','SumTwoDays')
+ThreeDays_dict = dataframe_to_dict(df1,'DATE','SumThreeDays')
+SixDays_dict = dataframe_to_dict(df1,'DATE','SumSixDays')
+TenDays_dict = dataframe_to_dict(df1,'DATE','SumTenDays')
+
+
+#### Create a pandas dataframe
+df = pd.DataFrame(data)
+df1 = pd.DataFrame(raindata)
 
 
 # Geometric Mean
@@ -56,37 +63,26 @@ def stv(iterable):
     eq = 10 ** (st.mean(b) + (1.282 * st.stdev(b)))
     return eq
 
-# Create a list of lists for the bacteria, location, month, etc, to put into a pandas dataframe
+# Average
+def average(iterable):
+    a = np.array(iterable)
+    eq = (a.sum())/(len(iterable))
+    return eq
 
+# Create a list of lists for the bacteria, location, month, etc
 LIST = []
 
-#using the month, bacteria, and location, get the geometric mean
+# using the month, bacteria, and location, get the geometric mean
 def pullingdata(month,year,bacteria,locationID):
     # Matching the correct columns with the parameters from the function
-
-    ## PWD Data
-    #DATA = (df.loc[(df['sample.date'].dt.month == (month))
-     #                  & (df['sample.date'].dt.year == (year))
-      #                 & (df['parameter'] == (bacteria))
-       #                & (df['loc.ID'] == (locationID))])
-
-    ## DRBC Data
-    DATA = (df.loc[(df['ActivityStartDate'].dt.month == (month))
-                   & (df['ActivityStartDate'].dt.year == (year))
-                   & (df['CharacteristicName'] == (bacteria))
+    DATA = (df.loc[(df['Date'].dt.month == (month))
+                   & (df['Date'].dt.year == (year))
+                   & (df['Bacteria'] == (bacteria))
                    & (df['Location'] == (locationID))])
 
-    #print(DATA)
-
     # Make a list of all of the data (MPN/100mL) with the same criteria (month, bacteria, and location iD
-
-    ## PWD Data
-    #DataList = DATA['data.value'].tolist()
-    #DayList = DATA['sample.date'].dt.day.tolist()
-
-    ## DRBC Data
-    DataList = DATA['ResultMeasureValue'].tolist()
-    DayList = DATA['ActivityStartDate'].dt.day.tolist()
+    DataList = DATA['Data'].tolist()
+    DayList = DATA['Date'].dt.day.tolist()
 
     # If there is no data for the month, only print this statement and do nothing else
     if not DataList:
@@ -98,9 +94,10 @@ def pullingdata(month,year,bacteria,locationID):
         print("Month/Year: " + str(month) + "/" + str(year))
         print(DataList)
         print(DayList)
+        print(average(DataList))
         print("Only one data point - cannot calculate STV, GM, or percentiles\n")
         x = 'NA'
-        return LIST.append([month, year, locationID, bacteria,x,x,x,x,x,DataList[0],x,x,x,DataList,DayList])
+        return LIST.append([month, year, locationID, float(average(DataList)), bacteria,x,x,x,x,x,DataList[0],x,x,x,DataList,DayList])
 
     # If there is data for the month, make a list of the month, location, bacteria, and the geometric mean
     else:
@@ -109,6 +106,7 @@ def pullingdata(month,year,bacteria,locationID):
         print("Month/Year: " + str(month) + "/" + str(year))
         print(DataList)
         print(DayList)
+        print(average(DataList))
         print("Geometric Mean: " + str(geo_mean(DataList)))
         print("STV: " + str(stv(DataList)) + "\n")
         p0 = np.percentile(DataList, 0)
@@ -118,62 +116,16 @@ def pullingdata(month,year,bacteria,locationID):
         p75 = np.percentile(DataList, 75)
         p90 = np.percentile(DataList, 90)
         p100 = np.percentile(DataList, 100)
-        return LIST.append([month, year, locationID, bacteria, float(geo_mean(DataList)), float(stv(DataList)),
+        return LIST.append([month, year, locationID, bacteria, float(average(DataList)), float(geo_mean(DataList)), float(stv(DataList)),
                             float(p0),float(p5),float(p25),float(p50),float(p75),float(p90),float(p100),
                             DataList,DayList])
 
-DAILYLIST = []
-
-#using the month, bacteria, and location, get the RAINFALL VALUES
-def dailydata(month,year,bacteria,locationID):
-    # Matching the correct columns with the parameters from the function
-
-    ## PWD Data
-    #DATA = (df.loc[(df['sample.date'].dt.month == (month))
-    #                   & (df['sample.date'].dt.year == (year))
-    #                   & (df['parameter'] == (bacteria))
-    #                   & (df['loc.ID'] == (locationID))])
-
-    ## DRBC Data
-    DATA = (df.loc[(df['ActivityStartDate'].dt.month == (month))
-                   & (df['ActivityStartDate'].dt.year == (year))
-                   & (df['CharacteristicName'] == (bacteria))
-                   & (df['Location'] == (locationID))])
-
-    # Make a list of all of the data (MPN/100mL) with the same criteria (month, bacteria, and location iD
-
-    ## PWD Data
-    #DataList2 = DATA['data.value'].tolist()
-    #DayList2 = DATA['sample.date'].tolist()
-
-    ## DRBC Data
-    DataList2 = DATA['ResultMeasureValue'].tolist()
-    DayList2 = DATA['ActivityStartDate'].tolist()
-
-    # If there is no data for the month, only print this statement and do nothing else
-    if not DataList2:
-        print("No data for month " + str(month) + "\n")
-
-    # If there is data for the month, make a list of the month, location, bacteria, and the geometric mean
-    else:
-        print(DataList2)
-        print(DayList2)
-        return DAILYLIST.append(DayList2)
-
 # Make a list of only the unique locations and bacteria types
-
-## PWD Data
-#LocationList = df['loc.ID'].unique().tolist()
-#BacteriaList = df['parameter'].unique().tolist()
-#YearList = df['sample.date'].dt.year.unique().tolist()
-
-## DRBC Data
 LocationList = df['Location'].unique().tolist()
-BacteriaList = df['CharacteristicName'].unique().tolist()
-YearList = df['ActivityStartDate'].dt.year.unique().tolist()
+BacteriaList = df['Bacteria'].unique().tolist()
+YearList = df['Date'].dt.year.unique().tolist()
 
 i=0
-
 #loop through all the years
 for Y in range(len(YearList)):
     # outer loop is for all the months
@@ -184,59 +136,29 @@ for Y in range(len(YearList)):
             for B in range(len(BacteriaList)):
                 # running the above function
                 pullingdata(i,YearList[Y],BacteriaList[B],LocationList[L])
-                dailydata(i,YearList[Y],BacteriaList[B],LocationList[L])
         i+=1
 
 
-#make a list of lists for all of the rain values
-totalrainlist = []
-for NestedList in DAILYLIST:
-    rainelements = []
-    for DateInNestedList in NestedList:
-        y = rain_dict.get(DateInNestedList)
-        rainelements.append(y)
-    totalrainlist.append(rainelements)
-
-
-# get the longest list in the list of rain values
-def longest(list):
-    list_len = [len(i) for i in list]
-    return (max(list_len))
-
-# make a list of strings the length of rain values
-columnsforrain = ["Rain"]*longest(totalrainlist)
-
-
 #### Create a Pandas Dataframe of the data.
-pwd_data = pd.DataFrame(columns = ["MONTH", "YEAR", "LocationID","Bacteria","GEO MEAN","STV",
+exportingdata = pd.DataFrame(columns = ["MONTH", "YEAR", "LocationID","Bacteria","Average","GEO MEAN","STV",
                                    "P0","P5","P25","P50","P75","P90","P100",
                                    "Data List","Day List"])
-#### Create a Pandas Dataframe of the RAIN data - columns automatically created for the length of the longest rain list
-pwd2_data = pd.DataFrame(columns = columnsforrain)
+
 
 # make the earlier defined list into a dataframe
-pwd_data = pwd_data.append(pd.DataFrame(LIST, columns = pwd_data.columns))
-pwd2_data = pwd2_data.append(pd.DataFrame(totalrainlist, columns = pwd2_data.columns))
-
-# join the two data frames
-pwd_data = pd.concat([pwd_data,pwd2_data],axis=1)
-
+exportingdata = exportingdata.append(pd.DataFrame(LIST, columns = exportingdata.columns))
 
 
 ##### EXPORT TO EXCEL
 
-# determining the name of the file
-## PWD Data
+### CHANGE THIS TO NAME
 #file_name = 'PWDdata.xlsx'
-
-## DRBC Data
 file_name = 'DRBC2data.xlsx'
 
-
 # saving the excel
-pwd_data.to_excel(file_name)
-
+exportingdata.to_excel(file_name)
 print('DataFrame is written to Excel File successfully.')
+
 
 
 
