62ed976e5957dd8be59e84c92e4fdb21f2ab2142
saffronl <51935852+saffronl@users.noreply.github.com>
2022-03-22 21:40:29 -0400

    Update DistributionTests.py

--- DistributionTests.py
+++ DistributionTests.py
@@ -2,54 +2,29 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import stats
-#from scipy.stats import normaltest
-
-## For other excel files - all of the column names will have to change to fit the new excel sheet
-#   'sample.date', 'parameter', 'loc.ID', 'data.value'
 
 #### Reading from the excel sheet
-data = pd.read_excel(r'/Users/saffron/Documents/Howard_Neukrug_Water_Center_PWD_Shore_Grab_Bacteria_2019.xlsx')
+data = pd.read_excel(r'/Users/saffron/Documents/Water Research/DRBC 2019,2020,2021/DRBC 19,20,21.xlsx')
 #print(data)
-
-## DRBC Data
-#data = pd.read_excel(r'/Users/saffron/Downloads/Nearshore_Bacteria.xlsx',sheet_name = "Raw Data")
 
 #### Create a pandas dataframe
 df = pd.DataFrame(data)
 
-#### Remove missing data from IMPORTANT columns
-df = df.dropna(subset=['sample.date', 'parameter', 'loc.ID', 'data.value'])
-
-## DRBC Data
-#df = df.dropna(subset=['ActivityStartDate', 'CharacteristicName', 'Location', 'ResultMeasureValue'])
-
-#& (df['loc.ID'] == ('SC_BANKS'))
-Data = df.loc[(df['parameter'] == ('E.coli'))]
-              #& (df['sample.date'].dt.month == (6))]
-#Data = df.loc[(df['CharacteristicName'] == ('Fecal Coliform'))]
-
-
+# only get e.coli data
+Data = df.loc[(df['Bacteria'] == ('E.Coli'))]
 #print(Data)
 
-DataList = []
+# turn the bacteria data into a list
+DataList = Data['Data'].tolist()
 
-for k in Data:
-    DataList = Data['data.value'].tolist()
-    #DataList = Data['ResultMeasureValue'].tolist()
+# take the log of every bacteria concentration
+LogData = np.log10(DataList).tolist()
 
-
-LogData = []
-for i in DataList:
-    LogData = np.log(DataList).tolist()
-
+# option to plot the data on a histogram:
 #plt.hist(LogData, bins=25)
 #plt.show()
 
-DataList = LogData
-
-#plt.hist(DataList, bins=100)
-#plt.show()
-
+# list of distributions in scipy
 list_of_dists = ['alpha', 'anglit', 'arcsine', 'beta', 'betaprime', 'bradford', 'burr', 'burr12', 'cauchy', 'chi',
                  'chi2', 'cosine', 'dgamma', 'dweibull', 'erlang', 'expon', 'exponnorm', 'exponweib', 'exponpow', 'f',
                  'fatiguelife', 'fisk', 'foldcauchy', 'foldnorm', 'frechet_r', 'frechet_l', 'genlogistic', 'genpareto',
@@ -64,13 +39,14 @@ list_of_dists = ['alpha', 'anglit', 'arcsine', 'beta', 'betaprime', 'bradford', 
 
 results = []
 
+# fit every distribution to the dataset
 for i in list_of_dists:
     dist = getattr(stats, i)
-    param = dist.fit(DataList)
-    a = stats.kstest(DataList, i, args=param)
+    param = dist.fit(LogData)
+    a = stats.kstest(LogData, i, args=param)
     results.append((i, a[0], a[1]))
 
-
+# sort the results, best fitting is on the top
 results.sort(key=lambda x: float(x[2]), reverse=True)
 for j in results:
     print("{}: statistic= {}, pvalue= {}".format(j[0], j[1], j[2]))
